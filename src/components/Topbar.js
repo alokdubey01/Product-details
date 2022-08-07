c1aec3a3cdb07ac4032e2b0d896cf2f1d824de30
Alok Kumar Dubey <72490595+alokdubey01@users.noreply.github.com>
2022-08-07 20:31:43 +0530

    Delete Topbar.js

--- src/components/Topbar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Topbar() {
-  return (
-    <div className='relative p-4 bg-indigo-500 block'>
-        <h1 className='text-white font-semibold text-lg text-center'>Yaariscore.me</h1>
-    </div>
-  )
-}
